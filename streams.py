8ea7f0011a3bab69641e924c8286bb7364538ed6
Hendrik Sollich <hendrik.sollich@gmx.de>
2010-11-08 12:55:54 +0100

    initial gui prototyp

---
+++ streams.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+import xml.parsers.expat
+import gtk
+
+class controller:
+	def __init__(self):
+		sfr = streamFileReader()
+		gui = gladeGUI(sfr)
+		gtk.main()
+
+class gladeGUI:
+	def __init__(self,model):
+		self.model = model
+		
+		self.guifile = "streamsgui.glade"
+		self.builder = gtk.Builder()
+		self.builder.add_from_file(self.guifile)
+		self.init_ui()
+
+		self.builder.connect_signals(self)
+		self.window.show()
+	
+	def init_ui(self):
+		self.delete_dialog = self.builder.get_object("messagedialog1")
+		self.window = self.builder.get_object("window1")
+		self.entry_url = self.builder.get_object("entry_url")
+		self.cbox_mimes = self.builder.get_object("combobox_mimes")
+		self.cbox_urls = self.builder.get_object("combobox_urls")
+		
+		self.populate_cbox(self.model.mimes,'combobox_mimes')
+		self.populate_cbox(self.model.urls,'combobox_urls')
+
+
+	def populate_cbox(self,values,objectname):
+		liststore = gtk.ListStore(str)
+		combobox = self.builder.get_object(objectname)
+		combobox.set_model(liststore)
+		cell = gtk.CellRendererText()
+		combobox.pack_start(cell,True)
+		combobox.add_attribute(cell, 'text', 0)
+
+		for value in values:
+			liststore.insert(0,[value])
+		combobox.set_active(0)
+
+	def on_button_delete_clicked(self,window):
+		self.delete_dialog.run()
+
+	def on_button_save_clicked(self,window):
+		print gtk.Entry.get_text(self.entry1)
+
+	def on_button_add_clicked(self,window):
+		print gtk.Entry.get_text(self.entry1)
+
+	def on_combobox_urls_changed(self,window):
+		url = self.cbox_urls.get_active_text()
+		active = self.cbox_urls.get_active()
+		self.entry_url.set_text(url)
+
+	def on_window1_destroy(self,window):
+		exit()
+
+class streamFileReader:
+	streams = []
+	mimes = ['audio/x-mpegurl','audio/mpeg','audio/x-scpls','application/ogg']
+	urls = []
+
+	def __init__(self):
+		self.p = xml.parsers.expat.ParserCreate()
+		self.file = self.loadStream()
+		self.p.StartElementHandler = self.s_e_h
+		
+		if self.file:
+			self.parsed = self.p.Parse(self.file)
+			for stream in self.streams:
+				self.urls.append(stream['url'])
+		else:
+			print 'couldn\'t load'
+
+	def s_e_h(self, name, attrs):
+		if name == 'stream':
+			self.streams.append(attrs)
+	
+	def get_mimes(self):
+		mimes = []
+		for s in self.streams:
+			mimes.append(s['mimetype'])
+		return mimes
+
+	def loadStream(self):
+		with open('./url_streams') as file:
+			print 'file loaded'
+			return file.read()
+	
+if __name__ == '__main__':
+	tool = controller()
